a5d66531b5f4612cf308df4af1636c962a8b364a
Che-Liang Chiou <clchiou@gmail.com>
2013-07-15 03:40:01 -0700

    Version 0.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='ctypes-binding-generator',
-    version='0.0.1',
+    version='0.1.0',
     description='Generate ctypes binding from C source files',
     author='Che-Liang Chiou',
     author_email='clchiou@gmail.com',
