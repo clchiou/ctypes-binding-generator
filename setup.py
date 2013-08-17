93a19a946e3ce6b3008f81e82ed395030bf9f8db
Che-Liang Chiou <clchiou@gmail.com>
2013-08-16 22:01:19 -0700

    Add info to setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ setup(
     description='Generate ctypes binding from C source files',
     author='Che-Liang Chiou',
     author_email='clchiou@gmail.com',
+    license='GNU GPLv3',
+    url='https://github.com/clchiou/ctypes-binding-generator',
     packages=['cbind', 'cbind/passes'],
     scripts=['bin/cbind'],
 )
